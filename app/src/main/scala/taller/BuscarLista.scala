6620a461c4aa0d3d3d5e1d4802a3479b63e2214b
Pablo Nicolas Marin Gonzalez <103337571+Slylem0@users.noreply.github.com>
2025-10-05 19:05:26 -0500

    Add ConjDifusoOps object with set operations

--- app/src/main/scala/taller/BuscarLista.scala
+++ app/src/main/scala/taller/BuscarLista.scala
@@ -2,9 +2,18 @@ package taller
 
 class BuscarLista() {
   def buscarElemento(lista: List[Int], elemento: Int): Boolean = {
-  lista match {
-    case Nil => false
-    case x :: xs => if (x == elemento) true else buscarElemento(xs, elemento)
-   }
+    lista match {
+      case Nil => false
+      case x :: xs => if (x == elemento) true else buscarElemento(xs, elemento)
+    }
   }
 }
+
+// ----------------------------------------------------------------------------------------------------------
+object ConjDifusoOps {
+  type ConjDifuso = Int => Double
+  def complemento(c: ConjDifuso): ConjDifuso = (x: Int) => 1.0 - c(x)
+  def union(cd1: ConjDifuso, cd2: ConjDifuso): ConjDifuso = (x: Int) => math.max(cd1(x), cd2(x))
+  def interseccion(cd1: ConjDifuso, cd2: ConjDifuso): ConjDifuso = (x: Int) => math.min(cd1(x), cd2(x))
+}
+// -------------------
