16a93f185ecc4d8c7282a06f2c23dfda5d1998ed
Pablo Nicolas Marin Gonzalez <103337571+Slylem0@users.noreply.github.com>
2025-11-01 11:58:07 -0500

    Refactor InclusionIgualdad to use function type

--- app/src/main/scala/taller/InclucionIgualdad.scala
+++ app/src/main/scala/taller/InclucionIgualdad.scala
@@ -2,11 +2,9 @@ package taller
 
 import scala.annotation.tailrec
 
-abstract class ConjDifuso {
-  def pertenece(elem: Int): Double
-}
-
 object InclusionIgualdad {
+  
+  type ConjDifuso = Int => Double
 
   // Inclusión usando recursión de cola
   def inclusion(cd1: ConjDifuso, cd2: ConjDifuso): Boolean = {
@@ -14,7 +12,7 @@ object InclusionIgualdad {
     def verificarInclusion(n: Int): Boolean = {
       if (n > 1000)
         true
-      else if (cd1.pertenece(n) > cd2.pertenece(n))
+      else if (cd1(n) > cd2(n))
         false
       else
         verificarInclusion(n + 1)
